06bac764b5d819b70e27a46e005f0b2a9bd1ba79
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-06-11 22:21:19 +0530

    BUG update

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+console.log("ml5 version: ", ml5.version);
+
+status1 = false;
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
+  canvas = createCanvas(400, 400);
+  canvas.center();
+
+  capture = createCapture(VIDEO);
+  capture.size(400, 400);
+  capture.hide();
+}
+
+function start() {
+  objectDetector = ml5.objectDetector('cocossd', modelLoded);
+  document.getElementById('status').innerHTML="Status: Detecting Objects";
+  object_name = document.getElementById('Object_name').value;
+}
+
+function modelLoded() {
+  console.log('Model Loaded!');
+  status1 = true;
+}
+
 function draw() {
-    background(220);
+  image(capture, 0, 0, 400, 400);
 }
