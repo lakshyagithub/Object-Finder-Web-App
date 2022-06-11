7b8c805e40ccf1212589981a9a969def4f89bcf4
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-06-11 22:37:10 +0530

    Bug fix update!

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-console.log("ml5 version: ", ml5.version);
-
-status1 = false;
+Status = "";
+input_text = "";
 
 function setup() {
   canvas = createCanvas(400, 400);
   canvas.center();
-
-  capture = createCapture(VIDEO);
-  capture.size(400, 400);
-  capture.hide();
+  video = createCapture(VIDEO);
+  video.size(400, 400);
+  video.hide();
 }
 
 function start() {
-  objectDetector = ml5.objectDetector('cocossd', modelLoded);
-  document.getElementById('status').innerHTML="Status: Detecting Objects";
-  object_name = document.getElementById('Object_name').value;
+  detector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById('status').innerText="Status: Detecting Objects";
+  input_text = document.getElementById('input').value;
 }
 
-function modelLoded() {
-  console.log('Model Loaded!');
-  status1 = true;
+function modelLoaded() {
+  console.log("model loaded");
+  Status = true;
 }
 
 function draw() {
-  image(capture, 0, 0, 400, 400);
+  image(video, 0, 0, 400, 400);
 }
